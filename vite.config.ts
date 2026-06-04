aa10e56481fb4a5fb4bf63acd698df5192fb8f20
Andrea Leoni <userpeaktalent@gmail.com>
2026-06-04 12:02:12 +0200

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production';
   return {
+    cacheDir: '.vite-cache',
     server: {
       port: 3000,
       host: '0.0.0.0',
